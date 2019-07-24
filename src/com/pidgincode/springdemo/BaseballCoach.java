d89035b6f588857268377290af5f2ec8e74c6191
Collins Ushi <collinsushi@gmail.com>
2019-07-24 16:19:57 +0100

    Complete baseball player gets a daily workout instruction from his coach

---
+++ src/com/pidgincode/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.pidgincode.springdemo;
+
+public class BaseballCoach {
+	
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on the batting practice";
+	}
+	
+}
---
+++ src/com/pidgincode/springdemo/MyApp.java
@@ -0,0 +1,15 @@
+package com.pidgincode.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		//create object
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		//Use object
+		System.out.println(theCoach.getDailyWorkout());
+		
+
+	}
+
+}
